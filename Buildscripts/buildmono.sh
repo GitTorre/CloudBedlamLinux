0d09f023faf1a652580909b71361bbbc713edfb3
Patrick Butler Monterde <pbutlerm@gmail.com>
2017-05-18 08:52:29 -0700

    Updated comments

--- Buildscripts/buildmono.sh
+++ Buildscripts/buildmono.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "Hello World"
+echo "--------------------------------------"
+echo "Building CloudBedLam Mono version"                       "
+echo "--------------------------------------"
 echo "AGENT_WORKFOLDER is $AGENT_WORKFOLDER"
 echo "AGENT_WORKFOLDER contents:"
 ls -1 $AGENT_WORKFOLDER
@@ -10,10 +12,13 @@ echo "BUILD_SOURCESDIRECTORY is $BUILD_SOURCESDIRECTORY"
 echo "BUILD_SOURCESDIRECTORY contents:"
 ls -1 $BUILD_SOURCESDIRECTORY
 echo "--------------------------------------"
+echo " Checking Paths                       "
+echo "--------------------------------------"
+echo "Step Path:" & pwd
+cd $BUILD_SOURCESDIRECTORY
+echo "Build Path:" & pwd
+echo "--------------------------------------"
 echo " Building Solution                    "
 echo "--------------------------------------"
-pwd
-cd $BUILD_SOURCESDIRECTORY
-pwd
 msbuild CloudBedlam.sln
 echo "Over and out."
