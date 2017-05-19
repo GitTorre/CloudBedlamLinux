e72d1b91018254a0a97a6188e2b114c2873fa9e9
Patrick Butler Monterde <pbutlerm@gmail.com>
2017-05-19 13:10:49 -0700

    Updated Build number

--- Buildscripts/release-linux-x64-elf.sh
+++ Buildscripts/release-linux-x64-elf.sh
@@ -6,15 +6,15 @@ echo "--------------------------------------"
 export GITHUB_TOKEN=$GITHUB_TOKEN
 
 echo "Github Token: $GITHUB_TOKEN"
-echo "Build Number: $BuildID" 
+echo "Build Number: $BUILD_NUMBER" 
 
-echo "Calling github-release"
-github-release release \
-    --user GitTorre \
-    --repo CloudBedlamMono \
-    --tag v$BuildID \
-    --name "CloudBedlam-v$BuildID" \
-    --description "CloudBedLam Fault Injection project. Release v0.$BuildID" \
-    --pre-release
+#echo "Calling github-release"
+#github-release release \
+#    --user GitTorre \
+#    --repo CloudBedlamMono \
+#    --tag v$BuildID \
+#    --name "CloudBedlam-v$BuildID" \
+#    --description "CloudBedLam Fault Injection project. Release v0.$BuildID" \
+#    --pre-release
 
 echo "Job Completed!"
