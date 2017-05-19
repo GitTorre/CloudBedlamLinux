c91f2783cb963fb065b2ff46f6bcf8f12fe37636
Patrick Butler Monterde <pbutlerm@gmail.com>
2017-05-19 13:29:46 -0700

    Adding the File upload

--- Buildscripts/release-linux-x64-elf.sh
+++ Buildscripts/release-linux-x64-elf.sh
@@ -8,7 +8,6 @@ export GITHUB_TOKEN=$GITHUB_TOKEN
 echo "Github Token: $GITHUB_TOKEN"
 echo "Build Number: $BUILD_BUILDID" 
 
-echo "Calling github-release"
 github-release release \
     --user GitTorre \
     --repo CloudBedlamMono \
@@ -16,5 +15,17 @@ github-release release \
     --name "CloudBedlam-v0.$BUILD_BUILDID" \
     --description "CloudBedLam Fault Injection project. Release v0.$BUILD_BUILDID" \
     --pre-release
+
+echo "--------------------------------------"
+echo "Uploading the Release Files           "
+echo "--------------------------------------"
+
+./github-release upload \
+    --user GitTorre \
+    --repo CloudBedLamMono \
+    --tag v0.$BUILD_BUILDID \
+    --name "CloudBedlam-linux-amd64" \
+    --file /home/patrick/_work/1/s/CloudBedlam/bin/Debug/CloudBedlam
+
 
 echo "Job Completed!"
