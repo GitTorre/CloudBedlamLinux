3cbe6390d7bab76673fc19e0170deba52b4b6efe
Charles Torre <torre_charles@hotmail.com>
2017-05-24 12:23:41 -0700

    Added netem-ip-bandwidth, supports domain, port...

--- CloudBedlam/Bash/netem-ip-bandwidth.sh
+++ CloudBedlam/Bash/netem-ip-bandwidth.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 #shell call
-#sh netem-ip-bandwidth.sh www.bing.com 22 56Kbit 60
+#sh netem-ip-bandwidth.sh www.bing.com 22 56kbit 60
 
 interface=$(ip -o link show | awk '{print $2,$9}' | grep UP | awk '{str = $0; sub(/: UP/,"",str); print str}')
 
@@ -14,12 +14,25 @@ port="$2"
 rate="$3"
 duration="$4"
 
+echo "$interface"
+echo "$ip"
+echo "$rate"
+echo "$port"
+
+# we'll use iptables to deal with port referencing (see # port section below)...
+iptables -t mangle -F
 
 #rate limiting for a given ip address at a given rate
-tc qdisc del dev $interface root
-tc qdisc add dev $interface root handle 1: cbq avpkt 1000 bandwidth 100mbit
-tc class add dev $interface parent 1: classid 1:1 cbq rate $rate allot 1500 prio 5 bounded isolated
-tc filter add dev $interface parent 1: protocol ip prio 16 u32 match ip dst $ip flowid 1:1
+tc qdisc add dev $interface root handle 1: htb default 1
+tc class add dev $interface parent 1: classid 0:1 htb rate $rate
+tc filter add dev $interface parent 1: protocol ip prio 1 u32 match ip dst $ip flowid 1:1
+
+# port...
+iptables -A FORWARD -t mangle -p tcp --sport $port -j MARK --set-mark 5
+iptables -A OUTPUT -t mangle -p tcp --sport $port -j MARK --set-mark 5
+
+iptables-save
+
 
 sleep $duration
 # delete existing filter rules, etc...
--- CloudBedlam/Bash/netem-ip-latency.sh
+++ CloudBedlam/Bash/netem-ip-latency.sh
@@ -18,6 +18,7 @@ iptables -t mangle -F
 
 tc qdisc add dev $interface root handle 1: prio
 tc filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip dst $ip flowid 2:1
+tc filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip src $ip flowid 2:1
 tc filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip sport $port 0xffff flowid 2:1
 tc qdisc add dev $interface parent 1:1 handle 2: netem delay $delay
 
