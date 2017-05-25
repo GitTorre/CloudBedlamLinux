39a070977531db2b6ed6aae9e931a76767269041
Charles Torre <torre_charles@hotmail.com>
2017-05-25 15:43:31 -0700

    Working

--- CloudBedlam/Bash/netem-ip-latency.sh
+++ CloudBedlam/Bash/netem-ip-latency.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# sh netem-ip-latency.sh www.bing.com 22 3000ms 30
+
 interface=$(ip -o link show | awk '{print $2,$9}' | grep UP | awk '{str = $0; sub(/: UP/,"",str); print str}')
 
 ip=$(host "$1" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" -m 1)
@@ -12,6 +14,8 @@ echo "$interface"
 echo "$ip"
 echo "$delay"
 echo "$port"
+
+tc qdisc del root dev $interface
 
 tc qdisc add dev $interface root handle 1: prio
 tc filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip dst $ip flowid 2:1
