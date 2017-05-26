067f039e92f0d5db3b72f84baf045553cc028ad5
Charles Torre <torre_charles@hotmail.com>
2017-05-26 15:04:58 -0700

    Supports multiple ips (Latency and Bandwidth netem)

--- CloudBedlam/Bash/netem-ip-latency.sh
+++ CloudBedlam/Bash/netem-ip-latency.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
-
-# sh netem-ip-latency.sh www.bing.com 22 3000ms 30
-
+# sh netem-ip-latency.sh -ips=... 3000ms 30
+# get the currently up and running network device...
 interface=$(ip -o link show | awk '{print $2,$9}' | grep UP | awk '{str = $0; sub(/: UP/,"",str); print str}')
-
-ip=$(host "$1" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" -m 1)
-
-port="$2"
-delay="$3"
-duration="$4"
-
-echo "$interface"
-echo "$ip"
-echo "$delay"
-echo "$port"
-
-#tc qdisc del root dev $interface
-
-tc qdisc add dev $interface root handle 1: prio
-tc filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip dst $ip flowid 2:1
-tc filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip src $ip flowid 2:1
+# vars
+delay="$2"
+duration="$3"
+TC=/sbin/tc 
+# Get the comma-delimited ip param value, set to var ipstring... 
+for i in "$@"
+do
+	case $i in
+		-ips=*)
+		ipstring="${i#*=}"
+		shift # past argument=value
+		;;
+		*)
+		        # unknown option
+		;;
+	esac
+done
+# Split ipstring, store into array ips
+declare -a ips
+ips=(${ipstring//,/ })
+# clear existing qdiscs... reinit... suppress error if file doesn't exist...
+$TC qdisc del dev $interface root    2> /dev/null > /dev/null
+$TC qdisc add dev $interface root handle 1: prio
+# loop through ips array and set shaping filters per ip, bound to dst/src cbq rl classes above...
+for ip in "${ips[@]}"
+do
+	$TC filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip dst $ip flowid 2:1
+	$TC filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip src $ip flowid 2:1
+    echo "Traffic to/from IP $ip is delayed by $delay"
+done
 tc qdisc add dev $interface parent 1:1 handle 2: netem delay $delay
-
 #tc filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip dport $port flowid 2:1
 #tc filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip sport $port flowid 2:1
-
-
 sleep $duration
 # delete existing filter rules, etc...
-tc qdisc del root dev $interface
+$TC qdisc del dev $interface root    2> /dev/null > /dev/null
