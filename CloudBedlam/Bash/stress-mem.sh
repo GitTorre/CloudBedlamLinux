9d057b03784c57ea00a4d6d3b0d1b63327575a76
Charles Torre <torre_charles@hotmail.com>
2017-05-27 12:55:29 -0700

    New mem alloc impl (CrunchMem), updated csproj(post-build command...).

--- CloudBedlam/Bash/stress-mem.sh
+++ CloudBedlam/Bash/stress-mem.sh
@@ -1,12 +1,9 @@
 #! /bin/bash
 
-pressurelevel=$(awk "BEGIN {printf \"%.2f\n\", $1/100}")
-vmbytes=$(awk "/MemFree/{printf \"%d\n\", \$2 * $pressurelevel;}" < /proc/meminfo)
+pressurelevel=$(awk "BEGIN {printf \"%.2f\", $1/100}")
+freemempt=$(awk "/MemFree/{printf \"%d\", \$2 * $pressurelevel;}" < /proc/meminfo)
 duration="$2"
-
+memtoeatmb=$(( $freemempt / 1024 ))
 echo $duration
-echo $pressurelevel
-echo $vmbytes
-
-
-/usr/bin/stress-ng --vm 4 --vm-bytes $vmbytes --mmap 2 --mmap-bytes 2G --page-in --timeout $duration
+echo $memtoeatmb
+./CrunchMem $memtoeatmb $duration
