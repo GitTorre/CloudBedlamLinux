f6e4c3cea16c4ed1d3046812d1547a6ebc8bdf81
Charles Torre <torre_charles@hotmail.com>
2017-06-02 16:58:22 -0700

    Replaces eatmem with stress (or stress-ng)
    eatmem isn't precise enough. stress works great (less so, stress-ng - it eats 100% of one core in this case (1 vm hog) for the duration of the run... stress does not incur this processing cost...). Maybe I am missing something... -CT

--- CloudBedlam/Bash/stress-mem.sh
+++ CloudBedlam/Bash/stress-mem.sh
@@ -1,7 +1,16 @@
 #! /bin/bash
 # nice and simple... :) -CT
 # bash stress-mem.sh 90 15 # eat 90% of available memory, stop after 15 seconds...
-ptpressure="$1%"
+#ptpressure= $1/100
+#duration= "$2"
+#echo "Eating $ptpressure of free memory (non-swap) for $duration"s...
+#./eatmem "$ptpressure" "$duration"
+# NOTE: stress will need to be installed. Uses less CPU than stress-ng, but in the case below, just one core is maxed out by stress-ng, 
+# none by stress... stress-ng should be installed already on Ubuntu, for example. Either way, choose one or the other. It's better than eatmem.c...
+pressurelevel=$(awk "BEGIN {printf \"%.2f\n\", $1/100}")
+vmbytes=$(awk "/MemFree/{printf \"%d\n\", \$2 * $pressurelevel;}" < /proc/meminfo)k 
 duration="$2"
-echo "Eating $ptpressure of free memory (non-swap) for $duration"s...
-./eatmem "$ptpressure" "$duration"
+echo $pressurelevel
+echo $vmbytes
+echo $duration
+stress --vm 1 --vm-bytes $vmbytes --vm-keep --vm-hang $duration --timeout "$duration"s
