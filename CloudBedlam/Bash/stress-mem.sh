cf5545a0af63d098813c92cb457d77892db94c68
Charles Torre <torre_charles@hotmail.com>
2017-06-02 17:26:52 -0700

    Update stress-mem.sh

--- CloudBedlam/Bash/stress-mem.sh
+++ CloudBedlam/Bash/stress-mem.sh
@@ -2,10 +2,6 @@
 
 # nice and simple... :) -CT
 # bash stress-mem.sh 90 15 # eat 90% of available memory, stop after 15 seconds...
-#ptpressure= "$1%"
-#duration= "$2"
-#echo "Eating $ptpressure of free memory for $duration"s...
-#./eatmem "$ptpressure" "$duration"
 
 pressurelevel=$(awk "BEGIN {printf \"%.2f\n\", $1/100}")
 vmbytes=$(awk "/MemFree/{printf \"%d\n\", \$2 * $pressurelevel;}" < /proc/meminfo)k 
