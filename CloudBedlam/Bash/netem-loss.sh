5f06052152f43c8d6c219934440c917a0a9197df
Charles Torre <torre_charles@hotmail.com>
2017-06-04 17:04:30 -0700

    burst

--- CloudBedlam/Bash/netem-loss.sh
+++ CloudBedlam/Bash/netem-loss.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 # bash netem-loss.sh -ips=... 5 30s # Random, 5% loss for specified ips for 30s...
-# 
+# To make this less random (and to emulate burst), add another percentage value after lossrate in the tc qdisc for loss... -CT
 interface=$(ip -o link show | awk '{print $2,$9}' | grep UP | awk '{str = $0; sub(/: UP/,"",str); print str}')
 # vars
 lossrate="$2%"
 duration="$3"
+burst=""
+if [ -n "$4" ]
+	then
+		burst="$4%"
+fi
 TC=/sbin/tc 
 # Get the comma-delimited ip param value, set to var ipstring... 
 for i in "$@"
@@ -30,15 +35,15 @@ $TC qdisc add dev $interface root handle 1: prio
 for address in "${ips[@]}"
 do
 	if [[ $address =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; #ipv4
-	then
-		$TC filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip dst $address flowid 1:1
-		$TC filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip src $address flowid 2:1
-		echo "Packets to/from IP $address set for random loss at $lossrate drop rate"
+		then
+			$TC filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip dst $address flowid 1:1
+			$TC filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip src $address flowid 2:1
+			echo "Packets to/from IP $address set for random loss with $lossrate drop rate (random means random...)"
 	fi
 
 done
 # create the qdisc under existing root qdisc... establishing delay using netem loss...
-$TC qdisc add dev $interface parent 1:1 handle 2: netem loss $lossrate
+$TC qdisc add dev $interface parent 1:1 handle 2: netem loss $lossrate $burst
 
 # keep configuration for the allotted time, then delete the qdiscs for $interface
 sleep $duration
