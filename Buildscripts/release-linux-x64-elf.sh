0ec8d074bf01a3a1b3908e9273507616ef4bd7c5
Charles Torre <torre_charles@hotmail.com>
2017-06-05 17:30:25 -0700

    Update release-linux-x64-elf.sh
    Renaming

--- Buildscripts/release-linux-x64-elf.sh
+++ Buildscripts/release-linux-x64-elf.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 echo "--------------------------------------"
 echo "Creating Release in GitHub Repo       "
@@ -10,10 +10,10 @@ echo "Build Number: $BUILD_BUILDID"
 
 github-release release \
     --user GitTorre \
-    --repo CloudBedlamMono \
+    --repo CloudBedlamLinux \
     --tag v0.$BUILD_BUILDID \
     --name "CloudBedlam-v0.$BUILD_BUILDID" \
-    --description "CloudBedLam Fault Injection project. Release v0.$BUILD_BUILDID" \
+    --description "CloudBedlam Fault Injection project. Release v0.$BUILD_BUILDID" \
     --pre-release
 
 echo "--------------------------------------"
@@ -22,7 +22,7 @@ echo "--------------------------------------"
 
 github-release upload \
     --user GitTorre \
-    --repo CloudBedLamMono \
+    --repo CloudBedlamLinux \
     --tag v0.$BUILD_BUILDID \
     --name "CloudBedlam-linux-amd64" \
     --file /home/patrick/_work/1/s/CloudBedlam/bin/Debug/CloudBedlam
